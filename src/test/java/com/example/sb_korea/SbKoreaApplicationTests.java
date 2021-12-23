ebe03c4d0f4698384e773d261c8cdef076d20edb
hao.wong <1393922403@qq.com>
2021-12-23 21:09:52 +0800

    feat: mockmvc annotation debug

--- src/test/java/com/example/sb_korea/SbKoreaApplicationTests.java
+++ src/test/java/com/example/sb_korea/SbKoreaApplicationTests.java
@@ -1,19 +1,21 @@
 package com.example.sb_korea;
 
 import com.alibaba.fastjson.JSON;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -22,6 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
 class SbKoreaApplicationTests {
 
     private static final Logger logger = LoggerFactory.getLogger(SbKoreaApplicationTests.class);
@@ -31,12 +35,13 @@ class SbKoreaApplicationTests {
     private MockMvc mvc ;
 
     /**
+     * 在Junit4下使用@Before和@After
+     * 而在Junit5下使用@BeforeEach和@AfterEach
      * @Description 初始化
      */
-    @Before
+    @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mvc = MockMvcBuilders.webAppContextSetup(context).build();  //构造MockMvc
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     /**
@@ -48,8 +53,7 @@ class SbKoreaApplicationTests {
      */
     public MvcResult doJson(String url, String authorization, Object obj){
         try {
-            MvcResult mvcResult =
-                    this.mvc.perform(MockMvcRequestBuilders.post(url)
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(url)
                                     .header("Authorization", authorization)
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .content(JSON.toJSONString(obj)))
@@ -72,8 +76,7 @@ class SbKoreaApplicationTests {
      */
     public MvcResult doFrom(String url, String authorization,  MultiValueMap<String, String> params){
         try {
-            MvcResult mvcResult =
-                    this.mvc.perform(MockMvcRequestBuilders.post(url)
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(url)
                                     .header("Authorization", authorization)
                                     .contentType(MediaType.ALL)
                                     .params(params))
@@ -90,8 +93,8 @@ class SbKoreaApplicationTests {
 
     @Test
     public void setPaperNameWindow(){
-        //实际请求
-//        MvcResult mvcResult = this.doJson(setPaperNameWindow, getAdminToken(1), vo);
+        MvcResult mvcResult = this.doFrom("/my", "", new LinkedMultiValueMap<>());
+        System.err.println();
     }
 
 }
